82ba2c1c3e6779fc5956ad820d3c4e4738832d6a
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-03-31 13:26:01 -0300

    Update lab19.rkt

--- lab19.rkt
+++ lab19.rkt
@@ -1,11 +1,14 @@
 #lang racket
-(define point% (class object%
-                 (super-new)
+(define point% (class object%(super-new)
                  (define/public (distance other)
                    (define other-x (send other get-x))
                    (define other-y (send other get-y))
                    (sqrt (+ (expt (- (send this get-x) other-x) 2)
                             (expt (- (send this get-y) other-y) 2))))
+                 (define/public (distance-range numIn)
+                   (cond
+                     [(>(send this distance)numIn)#f]
+                     [(<(send this distance)numIn)#t]))
                  (define/public (set-x nx) (set! x nx))
                  (define/public (set-y ny) (set! y ny))
                  (define/public (set-z nz) (set! z nz))
@@ -13,9 +16,8 @@
                  (define/public (get-y) y)
                  (define/public (get-z) z)
                  (init-field x y z)))
+                
 
 (define p1 (make-object point% 9 3 7))
 (define p2 (make-object point% -1 2 9))
-
-(display "Distance between p1 and p2: ")
 (displayln (send p1 distance p2))
