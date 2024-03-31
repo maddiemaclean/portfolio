c0624d70814fb2c485c23660ab61e16f668387e2
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-03-31 13:47:57 -0300

    Update lab19.rkt

--- lab19.rkt
+++ lab19.rkt
@@ -7,8 +7,10 @@
                             (expt (- (send this get-y) other-y) 2))))
                  (define/public (distance-range other numIn)
                    (cond
-                     [(>(send this distance (send other get-x))numIn)#f]
-                     [(<(send this distance (send other get-x))numIn)#t]))
+                     [(>(send this distance other)numIn)#f]
+                     [(<(send this distance other)numIn)#t]))
+                 (define/public (triangle pointB pointC)
+                   (+(send this distance pointB)(send this distance pointC)(send pointB distance pointC)))
                  (define/public (set-x nx) (set! x nx))
                  (define/public (set-y ny) (set! y ny))
                  (define/public (set-z nz) (set! z nz))
@@ -18,7 +20,9 @@
                  (init-field x y z)))
                 
 
-(define p1 (make-object point% 9 3 7))
-(define p2 (make-object point% -1 2 9))
+(define p1 (make-object point% 5 5 5))
+(define p2 (make-object point% 8 8 8))
+(define p3 (make-object point% 4 5 6))
 (displayln (send p1 distance p2))
-(displayln (send p1 distance-range p2 10))
+(displayln (send p1 distance-range p2 11))
+(displayln (send p1 triangle p2 p3))
