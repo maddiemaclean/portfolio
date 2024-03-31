b4ae26a3654e934f9ebb41242bb5857f3800cc83
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-03-31 12:15:32 -0300

    Update lab19.rkt

--- lab19.rkt
+++ lab19.rkt
@@ -1,20 +1,21 @@
 #lang racket
-(require racket/class)
-
 (define point% (class object%
                  (super-new)
                  (define/public (distance other)
-                 (get-dist) sqrt(+(expt(-(expt(send this get-x) other-x)2)(expt(-(send this get-y)other-y)2))));unbound identifier in other.x
-                 (define/public (set-x nx) (set! x nx)) ; should be (set! x nx) rather than (= x nx)
-                 (define/public (set-y ny) (set! y ny)) ; same as above but for y
+                   (define other-x (send other get-x))
+                   (define other-y (send other get-y))
+                   (sqrt (+ (expt (- (send this get-x) other-x) 2)
+                            (expt (- (send this get-y) other-y) 2))))
+                 (define/public (set-x nx) (set! x nx))
+                 (define/public (set-y ny) (set! y ny))
                  (define/public (set-z nz) (set! z nz))
-                 (define/public (get-x) x);missing accessors
+                 (define/public (get-x) x)
                  (define/public (get-y) y)
                  (define/public (get-z) z)
-                   (init-field x y z)))
+                 (init-field x y z)))
 
 (define p1 (make-object point% 9 3 7))
 (define p2 (make-object point% -1 2 9))
-  (cond
-  []
-  [else ]))
+
+(display "Distance between p1 and p2: ")
+(displayln (send p1 distance p2))
