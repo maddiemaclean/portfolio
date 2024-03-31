03fc6f676d0c57850f39b8d86c21b77dcda2c888
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-03-31 12:12:06 -0300

    Update lab19.rkt

--- lab19.rkt
+++ lab19.rkt
@@ -4,20 +4,17 @@
 (define point% (class object%
                  (super-new)
                  (define/public (distance other)
-                 (define/public (get-dist) sqrt (+ (expt (- (send this get-x) (send other get-x)) 2) (expt (- (send this get-y) (send other get-y)) 2)))) ; unbound identifier in other.x
+                 (get-dist) sqrt(+(expt(-(expt(send this get-x) other-x)2)(expt(-(send this get-y)other-y)2))));unbound identifier in other.x
                  (define/public (set-x nx) (set! x nx)) ; should be (set! x nx) rather than (= x nx)
                  (define/public (set-y ny) (set! y ny)) ; same as above but for y
                  (define/public (set-z nz) (set! z nz))
                  (define/public (get-x) x);missing accessors
                  (define/public (get-y) y)
                  (define/public (get-z) z)
-                   (init-field x y)))
+                   (init-field x y z)))
 
 (define p1 (make-object point% 9 3 7))
 (define p2 (make-object point% -1 2 9))
-(send p1 distance p2)
-
-(define (checkLength point1 point2 length)
   (cond
   []
   [else ]))
