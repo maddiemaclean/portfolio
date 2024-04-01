794edae61b8a200ddc122b8e289bf91f090c817b
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-04-01 11:24:24 -0300

    Update P3pt2.rkt

--- P3pt2.rkt
+++ P3pt2.rkt
@@ -1,23 +1,21 @@
 #lang racket
-(define BST% (class object%(init-field root)
+(define BST% (class object%(init root)
                (define/public (insert value)
-                  (define/public (insert-helper nodeIn)
+                  (define(insert-helper nodeIn)
                     (cond
                       [(null? nodeIn)(set! root (new LetterNode%(value value)))]
                       [(<value(send nodeIn get-letter))
-                       (cond (send nodeIn get-left))
-                        [(insert-helper(send nodeIn get-left))
-                         (send nodeIn set-left(new LetterNode%(value value)))]]
+                       (if (send nodeIn get-left)
+                        (insert-helper(send nodeIn get-left))
+                         (send nodeIn set-left(new LetterNode%(value value))))]
                       [(>value(send nodeIn get-letter))
-                       (cond (send nodeIn get-right))
-                        [(insert-helper(send nodeIn get-right))
-                         (send nodeIn set-right(new LetterNode%(value value)))]]
+                       (if (send nodeIn get-right)
+                        (insert-helper(send nodeIn get-right))
+                         (send nodeIn set-right(new LetterNode%(value value))))]
                       [else(send nodeIn increment-frequency)])))
 
                (define/public (print-tree nodeIn)
                  (unless(null? nodeIn)
                    (print-tree(send nodeIn get-left))
-                 (send nodeIn print)
-                 (unless(null? nodeIn)
-                   (print-tree(send node get-right))))
-                 (send nodeIn print)))))
+                   (send nodeIn print)
+                   (print-tree(send nodeIn get-right))))))
