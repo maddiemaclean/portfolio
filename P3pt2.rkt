5430f106a670bbb226b2eecdf56dc06322834b37
maddiemaclean <156142897+maddiemaclean@users.noreply.github.com>
2024-04-01 11:11:06 -0300

    Create P3pt2.rkt

---
+++ P3pt2.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(define BST% (class object%(init-field root)
+               (define/public (insert value)
+                  (define/public (insert-helper nodeIn)
+                    (cond
+                      [(null? nodeIn)(set! root (new LetterNode%(value value)))]
+                      [(<value(send nodeIn get-letter))
+                       (cond (send nodeIn get-left))
+                        [(insert-helper(send nodeIn get-left))
+                         (send nodeIn set-left(new LetterNode%(value value)))]]
+                      [(>value(send nodeIn get-letter))
+                       (cond (send nodeIn get-right))
+                        [(insert-helper(send nodeIn get-right))
+                         (send nodeIn set-right(new LetterNode%(value value)))]]
+                      [else(send nodeIn increment-frequency)])))
+
+               (define/public (print-tree nodeIn)
+                 (unless(empty?(send nodeIn get-left))
+                   (print-tree(send nodeIn get-left))
+                 (send nodeIn print)
+                 (unless(empty?(send nodeIn get-right))
+                   (print-tree(send node get-right)))))))
